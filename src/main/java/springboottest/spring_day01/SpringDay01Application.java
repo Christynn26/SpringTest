9696175c30b00c10aa4e370db5f8f8b14bac4b33
somin0016 <tsoominn@gmail.com>
2023-06-08 14:58:58 +0800

    first commit

---
+++ src/main/java/springboottest/spring_day01/SpringDay01Application.java
@@ -0,0 +1,15 @@
+package springboottest.spring_day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDay01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDay01Application.class, args);
+
+		System.out.println("Konnichiwa Christynn san");
+	}
+
+}
